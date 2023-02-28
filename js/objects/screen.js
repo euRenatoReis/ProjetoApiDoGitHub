f82420399c8311fbe8202f1f599305b8b9b2bc48
Renato Vieira <115581375+euRenatoReis@users.noreply.github.com>
2023-02-28 14:21:49 -0300

    Add files via upload

--- js/objects/screen.js
+++ js/objects/screen.js
@@ -10,9 +10,7 @@ const screen = {
                                                   <h1> ${user.name ?? "Nome de usuario não cadastrado"} </h1>
                                                   <p>  ${user.bio  ??  "Bio não cadastrada"} </p><br>
                                                   <p> Seguidores: ${user.followers ?? 'Numero De Seguidores Não Encontrado'} </p> <br>
-                                                  <p> Seguindo: ${user.following ?? 'Numero De Pessoas que Segue Não Encontrado'} </p> <br>
-                                                  <p> Numero de Eventos Create: ${user.eventsCreate ?? 'Numero de Eventos Do Tipo Create não Encontrado'}</p> <br>
-                                                  <p> Numero de Eventos Push: ${user.eventsPush ?? 'Numero de Eventos Do Tipo Push não Encontrado'}</p>  <br>
+                                                  <p> Seguindo: ${user.following ?? 'Numero De Pessoas que Segue Não Encontrado'} </p> <br>                                              
                                                </div> 
                                            </div>`;
 
@@ -21,34 +19,36 @@ const screen = {
             `<li><a href="${repo.html_url}" target="_blank"> ${repo.name} </a></li>
              <p> Numero de Estrelas: ${user.stargazers_count ?? 'Numero De Estrelas Não Encontrado'} </p> <br>
              <p> Numero de Forks: ${user.forks_count ?? 'Numero De Forks Não Encontrado'} </p> <br>
-             <p> Numero de watchers: ${user.watchers_count ?? 'Numero De Watchers Não Encontrado'} </p> <br>`)
-            
+             <p> Numero de watchers: ${user.watchers_count ?? 'Numero De Watchers Não Encontrado'} </p> <br>
+             <p> Numero de Eventos Create: ${user.CreateEvent ?? 'Numero de Eventos Do Tipo Create não Encontrado'}</p> <br>
+             <p> Numero de Eventos Push: ${user.PushEvent ?? 'Numero de Eventos Do Tipo Push não Encontrado'}</p>  <br>
+            `)
+
         if(user.repositories.length > 0){
             this.userProfile.innerHTML += `<div class="repositories section">
-                                                 <h2>Repositórios</h2>  
-                                                 
+                                                 <h2>Repositórios</h2>                                      
                                                  <ul>${repositoriesItems}</ul>
                                            </div> `
         }
 
         //a partir daqui é só gambiarra
         let eventsCreateOfProfile = '';
-        user.CreateEvent.forEach(event => eventsCreateOfProfile +=
-            `<li><a href='${event.html_url}' target='_blank' ${event.CreateEvent} </a></li>`)
+        repositories.CreateEvent.forEach(event => eventsCreateOfProfile +=
+            `<li><a href='${event.html_url}' target='_blank'> ${event.CreateEvent} </a></li>`)
 
-        if(user.CreateEvent.length > 0){
-            this.userProfile.innerHTML += `<div class='events section'
+        if(repositories.CreateEvent.length > 0){
+            this.userProfile.innerHTML += `<div class='events section'>
                                                 <h2> Eventos </h2>
                                                 <ul>${eventsCreateOfProfile}</ul>
                                            </div>`
         }                       
         
         let eventsPushOfProfile = '';
-        user.EventPush.forEach(event => eventsPushOfProfile +=
-            `<li><a href='${event.html_url}' target='_blank' ${event.PushEvent} </a></li>`)
+        repositories.PushEvent.forEach(event => eventsPushOfProfile +=
+            `<li><a href='${event.html_url}' target='_blank'> ${event.PushEvent} </a></li>`)
 
-        if(user.PushEvent.length > 0){
-            this.userProfile.innerHTML += `<div class='events section'
+        if(repositories.PushEvent.length > 0){
+            this.userProfile.innerHTML += `<div class='events section'>
                                                 <h2> Eventos </h2>
                                                 <ul>${eventsPushOfProfile}</ul>
                                            </div>`
